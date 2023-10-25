a82a1d400a7cac25d9f18377fe90e62d22dbfbb2
gentilegregoire <gregoire.gntile@gmail.com>
2023-10-25 16:59:02 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -19,16 +19,3 @@ $(function() {
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
-
-// Activate Carousel
-$("#myCarousel").carousel();
-
-// Enable Carousel Indicators
-$(".item").click(function(){
-  $("#myCarousel").carousel(1);
-});
-
-// Enable Carousel Controls
-$(".left").click(function(){
-  $("#myCarousel").carousel("prev");
-});
